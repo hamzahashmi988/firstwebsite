a04c445b9ac7cbf65f8407974a0ffe6e7b972438
MUHAMMAD HAMZA HASHMI <110339983+hamzahashmi988@users.noreply.github.com>
2023-09-20 16:54:34 +0500

    firstwebsite

---
+++ learn-to-code.js
@@ -0,0 +1,21 @@
+// var myAccountBalance= 300;
+// var nikeSBShoes=999.3;
+// var coupon=500;
+
+// if (nikeSBShoes <= myAccountBalance)
+// {
+// myAccountBalance -= nikeSBShoes;
+// console.log ("We just bought some dope shoes!"); 
+// console.log ("Account Balance:" + myAccountBalance);
+// }
+
+// else if(nikeSBShoes - coupon <= myAccountBalance){
+//     console.log ("We just bought some dope shoes!"); 
+//     myAccountBalance -= nikeSBShoes - coupon;
+//     console.log ("Account Balance: " + myAccountBalance);
+// }else{    console.log("you cant buy this shoes");
+//  }
+
+
+var students=["anees","anis","Anus"];
+console.log(students);
